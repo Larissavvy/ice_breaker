b7212026b81d70de884386222a26646d7438c368
Larissa Pereira <80949192+Larissavvy@users.noreply.github.com>
2024-08-25 23:22:42 +0530

    Add files via upload

--- icebreaker.py
+++ icebreaker.py
@@ -8,27 +8,30 @@ from langchain_core.output_parsers import StrOutputParser
 
 from linkedin_data import scrape_linkin_data
 from linkedin_lookup_agents import lookup
+from output_parsers import summary_parser
 
 
 def ice_break_with(name:str):
+    
     linked_in_user_name = lookup(name = name)
-    linked_in_data = scrape_linkin_data(profile_url=linked_in_user_name,test = True)
+    information = scrape_linkin_data(profile_url = 'https://in.linkedin.com/in/larissa0702',test = True)
 
+    
     summary_templete = """
             given the Linkedin information {information} about a person I want to create
             1. a short summary
             2. Major skills in the profile
-                          
+            
+            \n{format_instructions}
         """
     
-    summary_prompy_templete = PromptTemplate(input_variables=['information'],template=summary_templete)
+    summary_prompy_templete = PromptTemplate(input_variables=['information'],\
+                                template=summary_templete,\
+                                partial_variables={"format_instructions":summary_parser.get_format_instructions()})
 
     llm = ChatOpenAI(temperature= 0.6,model= "gpt-3.5-turbo")
 
-    chain = summary_prompy_templete | llm | StrOutputParser()
-
-    information = scrape_linkin_data(profile_url = 'https://in.linkedin.com/in/larissa0702')
-
+    chain = summary_prompy_templete | llm | summary_parser
     res = chain.invoke(input={'information':information})
 
     print(res)
@@ -39,11 +42,6 @@ if __name__ == "__main__":
     ice_break_with(name = 'Larissa Pereira Morgan Stanley Mumbai')
     
     
-    
-    
-
-
-#llm = ChatOllama(model= "llama3")
 
 
 
